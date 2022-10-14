6994a71ae71c2dd1c72f7462326414f222ae82a1
Tom Boddaert <70723965+tomBoddaert@users.noreply.github.com>
2022-10-14 15:16:40 +0100

    Added better error formatting

--- examples/fibonacci_from_nums.rs
+++ examples/fibonacci_from_nums.rs
@@ -8,13 +8,10 @@ fn main() {
     let memory = assembler::assemble_from_numbers(NUMBERS).unwrap();
 
     // Initialise the computer
-    let mut computer = runner::Computer {
-        memory,
-        ..Default::default()
-    };
+    let mut computer = runner::Computer::new(memory);
 
     // Run the computer
     if let Err(err) = runner::run(&mut computer) {
-        println!("{:?}", err);
+        println!("{err}");
     }
 }
